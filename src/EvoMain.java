f57dc131045c0582df7eefbd51a685256f101894
joe564338 <jmccully88@yahoo.com>
2017-02-25 02:08:44 -0800

    Finished world generator :)

--- src/EvoMain.java
+++ src/EvoMain.java
@@ -1,8 +1,13 @@
+import UI.MapComponents.Map.World;
+
 /**
  * Created by Joe on 2/2/2017.
  */
 public class EvoMain {
     public static void main(String [] args){
         System.out.println("Hello");
+        World world = new World(World.SMALL);
+        System.out.println(world.noiseToASCII());
+        System.out.println(world.worldToASCII());
     }
 }
--- src/UI/MapComponents/Map/World.java
+++ src/UI/MapComponents/Map/World.java
@@ -11,8 +11,11 @@ public class World {
     public static final int LARGE = 300;
     private int landMasses;
     Tile tiles[][];
+    int noise[][];
     Random random;
+    int size;
     public World(int size){
+        this.size = size;
         landMasses = 0;
         tiles = new Tile[size][size];
         //todo: generate structures with tiles...
@@ -22,17 +25,85 @@ public class World {
             }
         }
         random = new Random(System.currentTimeMillis());
-        int xCenter = random.nextInt(tiles.length);
-        int yCenter = random.nextInt(tiles[1].length);
-        recursiveGenerateLand(0, xCenter, yCenter);
-    }
-
-    private void recursiveGenerateLand(int previous, int x, int y){
-        if(previous == 0){
-            tiles[x][y] = new Tile(5, 100);
-            int xLength = random.nextInt(15) +5;
+        noise = new int[size][size];
+        int structures= 0;
+        for(int i = 0; i<size; i++){
+            for(int j = 0; j<size; j++){
+                noise[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < 16; i++){
+            int xRandom = random.nextInt(size);
+            int yRandom = random.nextInt(size);
+            noise[yRandom][xRandom] = 100;
+            recursivelyGenerateNoise(xRandom, yRandom);
+        }
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                int tileOk = random.nextInt(100);
+                if(noise[i][j] > tileOk) {
+                    tiles[i][j] = new Tile(5, 100);
+                }
+            }
         }
     }
+    public void recursivelyGenerateNoise(int initX, int initY){
+        recursivelyGenerateNoise(initX, initY, initX, initY, 0, 1);
+        recursivelyGenerateNoise(initX, initY, initX, initY, 1, 1);
+        recursivelyGenerateNoise(initX, initY, initX, initY, 1, 0);
+        recursivelyGenerateNoise(initX, initY, initX, initY, 1, -1);
+        recursivelyGenerateNoise(initX, initY, initX, initY, 0, -1);
+        recursivelyGenerateNoise(initX, initY, initX, initY, -1, -1);
+        recursivelyGenerateNoise(initX, initY, initX, initY, -1, 0);
+        recursivelyGenerateNoise(initX, initY, initX, initY, -1, 1);
+    }
+    public void recursivelyGenerateNoise(int initX, int initY,int currentX, int currentY, int deltaX, int deltaY){
+        if(Math.abs(currentX - initX) < 15 && Math.abs(currentY - initY) < 15){
+            if(currentX < 0 || currentY < 0 || currentX >= size || currentY >= size){
+
+                return;
+            }
+            noise[currentY][currentX] = 110 - (int)Math.sqrt(Math.pow(initX-currentX,2) + Math.pow(initY-currentY,2)) * 5;
+            if(deltaX == 0 && deltaY == 1) {
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 1, 0);
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 0, 1);
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, -1, 0);
+            }
+            else if(deltaX == 1 && deltaY == 1){
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 1, 1);
+            }
+            else if(deltaX == 1 && deltaY == 0){
+                //recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 0, 1);
+                //recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 0, -1);
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 1, 0);
+            }
+            else if(deltaX == 1 && deltaY == -1){
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 1, -1);
+            }
+            else if(deltaX == 0 && deltaY == -1){
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 0, -1);
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 1, 0);
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, -1, 0);
+            }
+            else if(deltaX == -1 && deltaY == -1){
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, -1, -1);
+            }
+            else if(deltaX == -1 && deltaY == 0){
+                //recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 0, 1);
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, -1, 0);
+                //recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, 0, -1);
+            }
+            else if(deltaX == -1 && deltaY == 1){
+                recursivelyGenerateNoise(initX, initY, currentX + deltaX, currentY + deltaY, -1, 1);
+            }
+
+        } else{
+            return;
+        }
+
+    }
+
+
     class Tile{
         int status;//Determines the type and color of tile; 0 for water, 1 for barren, 2, 3, 4, and 5 for food tiles where 5 has most food
         int food;
@@ -75,5 +146,29 @@ public class World {
             }
         }
     }
-
+    public String noiseToASCII(){
+        String noiseString = "";
+        for (int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                noiseString = noiseString + noise[i][j] + " ";
+            }
+            noiseString = noiseString + "\n";
+        }
+        return noiseString;
+    }
+    public String worldToASCII(){
+        String world = "";
+        for (int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(tiles[i][j].status > 0) {
+                    world = world + "L ";
+                }
+                else {
+                    world = world + "W ";
+                }
+            }
+            world = world + "\n";
+        }
+        return world;
+    }
 }
